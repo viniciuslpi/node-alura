a40fab9d9f63620aa59b42eee10b18487ac2230c
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-04-14 17:01:23 -0300

    async, promisses and regex

--- index.js
+++ index.js
@@ -5,14 +5,40 @@ function trataErro(erro){
   throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
 }
 
+async function pegaArquivo(caminhoDoArquivo){
+
+  const encoding = 'utf-8';
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(chalk.green(texto));
+  } catch(erro) {
+    trataErro(erro)
+  }
+
+}
+
+// \[[^\]]*\]
+// \(https?:\/\/[^$#\s].[^\s]*\)
+
+/*
 function pegaArquivo(caminhoDoArquivo){
   const encoding = 'utf-8'
-  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => { // '_' é usado para ignorar a primeira variavel
-    if(erro) { 
-      trataErro(erro); 
-    }
-    console.log(chalk.green(texto));
-  })
+  fs.promises.readFile(caminhoDoArquivo, encoding)
+    .then((texto) => chalk.green(console.log(texto)))
+    .catch((erro) => trataErro(erro))
+
 }
+
+function pegaArquivo(caminhoDoArquivo){
+const encoding = 'utf-8'
+fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => { // '_' é usado para ignorar a primeira variavel
+  if(erro) { 
+    trataErro(erro); 
+  }
+  console.log(chalk.green(texto));
+})
+} 
+
+*/
 
 pegaArquivo('./arquivos/texto1.md')
